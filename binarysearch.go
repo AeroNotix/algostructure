eeda906dbdbf1a83f2ed23f2bed3fac9bbf1842f
AeroNotix <aaron.l.france@gmail.com>
2012-08-30 09:04:56 +0200

    Added all futzing files

--- binarysearch.go
+++ binarysearch.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -36,46 +35,37 @@ func search(piece int, in []int) int {
 	return binsearch(piece, in, 0, len(in)-1)
 }
 
-func check_mid(n int, in []int) (int, error) {
-	if n == 0 {
-		return 0, errors.New("Piece doesn't exist in array")
-	}
-	return in[n], nil
-}
-
 func binsearch(piece int, in []int, ubound, lbound int) int {
-	mid := ((ubound + lbound) / 2)
+	mid := (ubound + lbound) / 2
 
-	Curr := in[mid]
+	if in[ubound] == piece {
+		return ubound
+	}
 
-	if Curr > piece {
-		return binsearch(piece, in, lbound, mid)
+	if in[lbound] == piece {
+		return lbound
 	}
-	if Curr < piece {
-		return binsearch(piece, in, mid, ubound)
-	}
-	if Curr == piece {
+
+	if in[mid] == piece {
 		return mid
 	}
 
-	if ubound - lbound <= 0 {
-		if in[ubound] == piece {
-			return ubound
-		}
-		if in[lbound] == piece {
-			return lbound
-		}
-		return -1
+	if lbound > ubound { return -1 }
+
+	if in[mid] > piece {
+		return binsearch(piece, in, mid, lbound)
 	}
-	
+
+	if in[mid] < piece {
+		return binsearch(piece, in, lbound, mid)
+	}
+
+
 	panic("Unreachable")
 }
 
 func main() {
 	do_tests()
-//	fmt.Println(search(3, []int{1,3,5}))
-	
-//	fmt.Println(search(0, []int{1,3,5}))
 }
 
 func do_tests() {
