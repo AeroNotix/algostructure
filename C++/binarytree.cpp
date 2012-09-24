f284343d8c138428078a69fc5fff0186e676401a
AeroNotix <aaron.l.france@gmail.com>
2012-09-24 14:08:29 +0000

    Templated binary tree

--- C++/binarytree.cpp
+++ C++/binarytree.cpp
@@ -4,109 +4,121 @@
 
 using namespace std;
 
-struct Tree {
-  int Value;
-  Tree *Left;
-  Tree *Right;
+template <class T>
+class Tree {
+public:
+	T Value;
+	Tree *Left;
+	Tree *Right;
 };
 
-Tree *NewNode(int i) {
-  Tree *t = new Tree;
-  t->Value = i;
-  t->Left = nullptr;
-  t->Right = nullptr;
-  return t;
+
+template <class T>
+Tree<T> *NewNode(T i) {
+	Tree<T> *t = new Tree<T>;
+	t->Value = i;
+	t->Left = nullptr;
+	t->Right = nullptr;
+	return t;
 }
 
+template <class T>
 class BinaryTree {
 
 public:
-  BinaryTree();
-  void Add(int);
-  vector<int> Walk();
-  unsigned int Levels() const;
+	BinaryTree();
+	void Add(T);
+	vector<T> Walk();
+	T Levels() const;
 
 private:
-  unsigned levels;
-  Tree *root;
-  void add(int i, Tree* &leaf);
-  vector<int> walk(Tree *leaf, vector<int>&);
+	unsigned levels;
+	Tree<T> *root;
+	void add(T i, Tree<T>* &leaf);
+	vector<T> walk(Tree<T> *leaf, vector<T>&);
 };
 
-BinaryTree::BinaryTree()
-  : root(nullptr), levels(0) {}
+template <class T>
+BinaryTree<T>::BinaryTree()
+	: root(nullptr), levels(0) {}
 
-unsigned int BinaryTree::Levels() const {
-  return levels;
+template <class T>
+T BinaryTree<T>::Levels() const {
+	return levels;
 }
 
-void BinaryTree::Add(int i) {
+template <class T>
+void BinaryTree<T>::Add(T i) {
 
-  levels++;
+	levels++;
 
-  if (root == nullptr) {
-    root = NewNode(i);
-    return;
-  }
+	if (root == nullptr) {
+		root = NewNode<T>(i);
+		return;
+	}
 
-  if (root->Value < i) {
-    return add(i, root->Right);
-  }
-  add(i, root->Left);
-  return;
+	if (root->Value < i) {
+		return add(i, root->Right);
+	}
+	add(i, root->Left);
+	return;
 }
 
-void BinaryTree::add(int i, Tree* &leaf) {
-  if (leaf == nullptr) {
-    leaf = NewNode(i);
-    return;
-  }
-  if (leaf->Value < i) {
-    return add(i, leaf->Right);
-  }
-  return add(i, leaf->Left);
+template <class T>
+void BinaryTree<T>::add(T i, Tree<T>* &leaf) {
+	if (leaf == nullptr) {
+		leaf = NewNode<T>(i);
+		return;
+	}
+	if (leaf->Value < i) {
+		return add(i, leaf->Right);
+	}
+	return add(i, leaf->Left);
 }
 
-vector<int> BinaryTree::Walk() {
+template <class T>
+vector<T> BinaryTree<T>::Walk() {
 
-  vector<int> out;
-  if (root == nullptr) {
-    return out;
-  }
+	vector<T> out;
+	if (root == nullptr) {
+		cout << "here\n";
+		return out;
+	}
 
-  walk(root->Left, out);
-  out.push_back(root->Value);
-  walk(root->Right, out);
+	walk(root->Left, out);
+	out.push_back(root->Value);
+	walk(root->Right, out);
 
-  return out;
+	return out;
 }
 
-vector<int> BinaryTree::walk(Tree *leaf, vector<int>& out) {
-  if (leaf == nullptr) {
-    return out;
-  }
+template <class T>
+vector<T> BinaryTree<T>::walk(Tree<T> *leaf, vector<T>& out) {
+	if (leaf == nullptr) {
+		return out;
+	}
 
-  walk(leaf->Left, out);
-  out.push_back(leaf->Value);
-  walk(leaf->Right, out);
+	walk(leaf->Left, out);
+	out.push_back(leaf->Value);
+	walk(leaf->Right, out);
 
-  return out;
+	return out;
 }
 
 int main() {
-  BinaryTree btree;
-  vector<int> tree;
+	BinaryTree<int> btree;
+	vector<int> tree;
 
-  btree.Add(0);
-  btree.Add(4);
-  btree.Add(5);
-  btree.Add(-2);
-  btree.Add(-1);
-  btree.Add(8);
+	btree.Add(0);
+	btree.Add(4);
+	btree.Add(5);
+	btree.Add(-2);
+	btree.Add(-1);
+	btree.Add(8);
 
-  tree = btree.Walk();
+	tree = btree.Walk();
 
-  for (auto x = tree.begin(); x != tree.end(); ++x) {
-    cout << (*x) << "\n";
-  }
+	for (auto x = tree.begin(); x != tree.end(); ++x) {
+		cout << (*x) << "\n";
+	}
 }
