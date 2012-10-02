40354f3bee4df6f2a7c908dc6593c7fa5f0c665e
AeroNotix <aaron.l.france@gmail.com>
2012-10-02 16:41:00 +0000

    Started a serious effort with the C++ prefix trie

--- C++/prefixtrie.cpp
+++ C++/prefixtrie.cpp
@@ -1,36 +1,149 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdexcept>
 
+template <class T>
+class Node {
 
-typedef Node struct {
-  std::string prefix
-  std::vector<Node*> Nodes
-  };
+private:
+    typedef typename T::iterator iter;
+    typedef T holding_type;
 
-int CompareStrings(std::string a, std::string b)
-{
-  std::string cmp;
-  if (a.length() < b.length()) {
-    cmp = a;
-  } else {
-    cmp = b;
-  }
+    T prefix;
+    std::vector<Node<T>> Nodes;
+    void Insert(iter a, iter b);
+    int DifferAt(T b);
+    int DifferAt(iter beg, iter end);
+    void Walk(std::vector<T> &out);
+    int levels = 0;
 
- 
-  int idx = 0;
-  for (;idx<cmp.length();++idx) {
-    if (a[idx] != b[idx]) {
-      break;
+public:
+    Node() {};
+    explicit Node(T pre) : prefix(pre) {};
+
+    int Levels() const { return levels; };
+    void Insert(T a);
+    std::vector<T> Walk();
+
+};
+
+template <class T>
+int Node<T>::DifferAt(T b) {
+
+    T cmp;
+    if (prefix.size() < b.size()) {
+	cmp = prefix;
+    } else {
+	cmp = b;
     }
-    std::cout << idx;
-  }
 
-  return idx;
+    int idx = 0;
+    for (; idx<cmp.size(); ++idx) {
+	if (prefix[idx] != b[idx]) {
+	    break;
+	}
+    }
+
+    return idx;
+}
+
+template <class T>
+int Node<T>::DifferAt(Node<T>::iter beg, Node<T>::iter end) {
+    auto nodebeg = prefix.begin();
+    int i = 0;
+    for (; nodebeg != prefix.end(); ++nodebeg) {
+	if (*beg == *nodebeg) {
+	    ++beg;
+	    ++i;
+	    continue;
+	}
+	break;
+    }
+    return i;
+}
+
+template <class T>
+void Node<T>::Insert(T newitem) {
+    ++levels;
+    Insert(newitem.begin(), newitem.end());
+}
+
+template <class T>
+void Node<T>::Insert(Node<T>::iter beg, Node<T>::iter end) {
+
+    if (beg == end) {
+	return;
+    }
+
+    for (auto nbeg = Nodes.begin(); nbeg != Nodes.end(); ++nbeg) {
+	int differ = nbeg->DifferAt(beg+prefix.size(), end);
+	if (differ > 0) {
+	    nbeg->Insert(beg+prefix.size()+differ, end);
+	    return;
+	}
+    }
+
+    Nodes.push_back(Node<T>(T(beg+DifferAt(beg, end), end)));
+}
+
+template <class T>
+std::vector<T> Node<T>::Walk() {
+    std::vector<T> out;
+    out.push_back(prefix);
+
+    for (auto nbeg = Nodes.begin(); nbeg != Nodes.end(); ++nbeg) {
+	nbeg->Walk(out);
+    }
+
+    return out;
+}
+
+template <class T>
+void Node<T>::Walk(std::vector<T> &out) {
+    out.push_back(prefix);
+
+    for (auto beg = Nodes.begin(); beg != Nodes.end(); ++beg) {
+	beg->Walk(out);
+    }
+}
+
+void PrintVec(std::vector<int> vec) {
+    std::cout << "[ ";
+    for (auto b = vec.begin(); b != vec.end(); ++b) {
+	std::cout << *b;
+    }
+    std::cout << " ]" << std::endl;
 }
 
 int main() {
-  std::cout << CompareStrings("Helllooooo", "Hell");
+
+    Node<std::string> n;
+
+    n.Insert("HIIIII");
+    n.Insert("HIIIIIa");
+    n.Insert("HIIIIIaa");
+    n.Insert("HIIIIIaaa");
+    n.Insert("HIIIIIaaaa");
+    n.Insert("something");
+
+    auto t = n.Walk();
+    for (auto b = t.begin(); b != t.end(); ++b) {
+	std::cout << *b << std::endl;
+    }
+
+    std::cout << n.Levels() << std::endl;
+
+    Node<std::vector<int>> vtrie;
+
+    vtrie.Insert({1,2,3,4,5});
+    vtrie.Insert({1,3,4,5,6});
+    vtrie.Insert({1,3,4,55,6});
+
+    std::vector<std::vector<int>> watr(vtrie.Walk());
+
+    for (auto b = watr.begin(); b != watr.end(); ++b) {
+	PrintVec(*b);
+    }
+    return 0;
 }
-
-
