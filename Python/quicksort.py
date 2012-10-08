27aad0fd4807ed65196fcd42decc5a5d7562b91b
AeroNotix <aaron.l.france@gmail.com>
2012-10-08 15:34:25 +0000

    Added Python directory with quicksort

---
+++ Python/quicksort.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+sys.setrecursionlimit(20000000)
+
+def quicksort(vec):
+    if len(vec) <= 1:
+        return vec
+
+    middlepos = len(vec) / 2
+    middle = vec[middlepos]
+
+    lesser = []
+    greater = []
+
+    vec = vec[:middlepos]+vec[middlepos+1:]
+
+    for el in vec:
+        if el <= middle:
+            lesser.append(el)
+            continue
+        greater.append(el)
+
+    return quicksort(lesser)+[middle]+quicksort(greater)
+
+if __name__ == '__main__':
+    vec = []
+    for x in range(1000000):
+        vec.append(random.randint(0,x))
+    
+    quicksort(vec)
