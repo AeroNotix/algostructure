1589459a1bc7f888c65e11929127e723b4ac91fa
AeroNotix <aaron.l.france@gmail.com>
2012-10-16 12:29:11 +0000

    Added binarytree C++/Python wrapper code

---
+++ CPython/binarytree/binarytree.cpp
@@ -0,0 +1,19 @@
+#include <Python.h>
+#include <iostream>
+#include "binarytree.h"
+
+static PyObject* binarytree_newtree(PyObject* self, PyObject *args) {
+    BinaryTree<PyObject*> n = BinaryTree<PyObject*>();
+    std::cout << "Hello, lulz" << std::endl;
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef binarytree_funcs[] = {
+    {"tree", (PyCFunction) binarytree_newtree, METH_VARARGS, NULL},
+    {NULL}
+};
+
+PyMODINIT_FUNC initbinarytree(void) {
+    Py_InitModule3("binarytree", binarytree_funcs, "Binary Tree wrapper module over a C++11 lib.");
+}
+
---
+++ CPython/binarytree/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+import os
+
+ROOT = os.path.dirname(__file__)
+
+module1 = Extension(
+    'binarytree',
+    include_dirs = [os.path.realpath(os.path.join(ROOT, os.pardir, os.pardir, "C++", "binarytree"))],
+    sources = ['binarytree.cpp'],
+    extra_compile_args = ['-std=c++11']
+    )
+
+setup(
+    name = 'binarytree',
+    author = "Aaron France",
+    author_email = "aaron.l.france@gmail.com",
+    version = '0.1',
+    description = "Binary Tree",
+    long_description = "Binary Tree. Written in C++11.",
+    ext_modules = [module1]
+    )
