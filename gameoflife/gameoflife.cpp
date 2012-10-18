cb597f8a2ec65dd462eec03fe43e1357b814078e
Aaron <aaron.l.france@gmail.com>
2012-10-18 18:58:54 +0200

    Use /dev/urandom as the source of randomness

--- gameoflife/gameoflife.cpp
+++ gameoflife/gameoflife.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fstream>
 
-#define HEIGHT 50
-#define WIDTH 50
-#define UPDATELENGTH 150000
+#define HEIGHT 80
+#define WIDTH 180
+#define UPDATELENGTH 100000
+#define ALIVE '*'
+#define DEAD ' '
 
 int board[HEIGHT][WIDTH];
 
-int weightedarray[10] = {0,0,0,0,0,0,0,1,1};
+int weightedarray[10] = {0,0,0,0,0,0,1,1,1};
 
 void genBoard(int (board)[HEIGHT][WIDTH]) {
+
+	std::ifstream ifs ("/dev/urandom", std::ifstream::in);
+
     for (int x = 0; x < HEIGHT; ++x) {
-	for (int y = 0; y < WIDTH; ++y) {
-	    board[x][y] = weightedarray[rand() % 10];
-	}
+		for (int y = 0; y < WIDTH; ++y) {
+			board[x][y] = weightedarray[(int) ifs.get() % 10];
+		}
     }
+	ifs.close();
 }
 
 void copyBoard(int (newB)[HEIGHT][WIDTH], int (oldB)[HEIGHT][WIDTH]) {
     for (int x = 0; x < HEIGHT; ++x) {
-	for (int y = 0; y < WIDTH; ++y) {
-	    newB[x][y] = oldB[x][y];
-	}
+		for (int y = 0; y < WIDTH; ++y) {
+			newB[x][y] = oldB[x][y];
+		}
     }
 }
 
@@ -30,86 +37,86 @@ void updateBoard(int (board)[HEIGHT][WIDTH]) {
     int next_board[HEIGHT][WIDTH];
     copyBoard(next_board, board);
     for (int x = 0; x < HEIGHT; ++x) {
-	for (int y = 0; y < WIDTH; ++y) {
+		for (int y = 0; y < WIDTH; ++y) {
 
-	    if ((x == 0 || x == HEIGHT) ||
-		(y == 0 || y == WIDTH)) {
-		continue;
-	    }
+			if ((x == 0 || x == HEIGHT) ||
+				(y == 0 || y == WIDTH)) {
+				continue;
+			}
+			
+			int aliveN = 0;
+			int deadN = 0;
+			if (!board[x-1][y+1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x][y+1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x+1][y+1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x+1][y]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x+1][y-1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x][y-1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x-1][y-1]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			if (!board[x-1][y]) {
+				deadN++;
+			} else {
+				aliveN++;
+			}
+			/*
+			  Rules
+			  =====
 
-	    int aliveN = 0;
-	    int deadN = 0;
-	    if (!board[x-1][y+1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x][y+1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x+1][y+1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x+1][y]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x+1][y-1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x][y-1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x-1][y-1]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    if (!board[x-1][y]) {
-		deadN++;
-	    } else {
-		aliveN++;
-	    }
-	    /*
-	      Rules
-	      =====
+			  1. Any live cell with fewer than two live neighbours
+			  dies, as if caused by under-population.
 
-	      1. Any live cell with fewer than two live neighbours
-	         dies, as if caused by under-population.
+			  2. Any live cell with two or three live neighbours lives
+			  on to the next generation.
 
-	      2. Any live cell with two or three live neighbours lives
-	         on to the next generation.
+			  3. Any live cell with more than three live neighbours
+			  dies, as if by overcrowding.
 
-	      3. Any live cell with more than three live neighbours
-	         dies, as if by overcrowding.
-
-	      4. Any dead cell with exactly three live neighbours
-	         becomes a live cell, as if by reproduction.
-	    */
-	    if ((board[x][y] && aliveN < 2)) {
-		next_board[x][y] = 0;
-		continue;
-	    }
-	    if ((board[x][y]) && ((aliveN == 2) || (aliveN == 3)))
-		continue;
-	    if ((board[x][y]) && (aliveN > 3)) {
-		next_board[x][y] = 0;
-		continue;
-	    }
-	    if ((!board[x][y]) && (aliveN == 3)) {
-		next_board[x][y] = 1;
-		continue;
-	    }
-	}
+			  4. Any dead cell with exactly three live neighbours
+			  becomes a live cell, as if by reproduction.
+			*/
+			if ((board[x][y] && aliveN < 2)) {
+				next_board[x][y] = 0;
+				continue;
+			}
+			if ((board[x][y]) && ((aliveN == 2) || (aliveN == 3)))
+				continue;
+			if ((board[x][y]) && (aliveN > 3)) {
+				next_board[x][y] = 0;
+				continue;
+			}
+			if ((!board[x][y]) && (aliveN == 3)) {
+				next_board[x][y] = 1;
+				continue;
+			}
+		}
     }
     copyBoard(board, next_board);
 }
@@ -117,24 +124,24 @@ void updateBoard(int (board)[HEIGHT][WIDTH]) {
 void drawBoard(int (board)[HEIGHT][WIDTH]) {
     std::cout << "\033[2J\033[1;1H";
     for (int x = 0; x < HEIGHT; ++x) {
-	for (int y = 0; y < WIDTH; ++y) {
-	    if (board[x][y] == 0) {
-		std::cout << ".";
-	    } else {
-		std::cout << "*";
-	    }
-	}
-	std::cout << std::endl;
+		for (int y = 0; y < WIDTH; ++y) {
+			if (board[x][y] == 0) {
+				std::cout << DEAD;
+			} else {
+				std::cout << ALIVE;
+			}
+		}
+		std::cout << std::endl;
     }
 }
-    
+
 
 int main() {
     genBoard(board);
     while (1) {
-	updateBoard(board);
-	drawBoard(board);
-	usleep(UPDATELENGTH);
+		updateBoard(board);
+		drawBoard(board);
+		usleep(UPDATELENGTH);
     }
     return 0;
 }
