0835b8166d3db41dc11099e3efbe3dd66af95958
AeroNotix <aaron.l.france@gmail.com>
2012-10-18 15:30:31 +0000

    Added rules in docstring

--- gameoflife/gameoflife.cpp
+++ gameoflife/gameoflife.cpp
@@ -21,57 +21,73 @@ void updateBoard(int (board)[HEIGHT][WIDTH]) {
 	for (int y = 0; y < WIDTH; ++y) {
 	    int aliveN = 0;
 	    int deadN = 0;
-	    if (board[x-1][y+1] == 0) {
+	    if (!board[x-1][y+1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x][y+1] == 0) {
+	    if (!board[x][y+1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x+1][y+1] == 0) {
+	    if (!board[x+1][y+1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x+1][y] == 0) {
+	    if (!board[x+1][y]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x+1][y-1] == 0) {
+	    if (!board[x+1][y-1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x][y-1] == 0) {
+	    if (!board[x][y-1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x-1][y-1] == 0) {
+	    if (!board[x-1][y-1]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if (board[x-1][y] == 0) {
+	    if (!board[x-1][y]) {
 		deadN++;
 	    } else {
 		aliveN++;
 	    }
-	    if ((board[x][y] == 1 && aliveN < 2)) {
+	    /*
+	      Rules
+	      =====
+
+	      1. Any live cell with fewer than two live neighbours
+	         dies, as if caused by under-population.
+
+	      2. Any live cell with two or three live neighbours lives
+	         on to the next generation.
+
+	      3. Any live cell with more than three live neighbours
+	         dies, as if by overcrowding.
+
+	      4. Any dead cell with exactly three live neighbours
+	         becomes a live cell, as if by reproduction.
+	    */
+	      if ((board[x][y] && aliveN < 2)) {
 		board[x][y] = 0;
 		continue;
 	    }
-	    if ((board[x][y] == 1) && ((aliveN == 2) || (aliveN == 3)))
+	    if ((board[x][y]) && ((aliveN == 2) || (aliveN == 3)))
 		continue;
-	    if ((board[x][y] == 1) && (aliveN > 3)) {
+	    if ((board[x][y]) && (aliveN > 3)) {
 		board[x][y] = 0;
 		continue;
 	    }
-	    if ((board[x][y] == 0) && (aliveN == 3)) {
+	    if ((!board[x][y]) && (aliveN == 3)) {
 		board[x][y] = 1;
 		continue;
 	    }
