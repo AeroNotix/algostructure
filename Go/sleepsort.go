a94af9f096935f49c18218435f22787ddba0f3a2
Aaron <aaron.l.france@gmail.com>
2012-10-22 21:06:08 +0200

    Added an exported SleepSort function

--- Go/sleepsort.go
+++ Go/sleepsort.go
@@ -6,9 +6,21 @@ import (
 	"time"
 )
 
+func SleepSort(arr []int) []int {
+	gather := make(chan int)
+	for _, num := range arr {
+		emit(num, gather)
+	}
+	out := make([]int, 0, len(arr))
+	for x := 0; x < 10; x++ {
+		out = append(out, <-gather)
+	}
+	return out
+}
+
 func emit(val int, to chan int) {
 	go func() {
-		ch := time.After(time.Duration(val) * time.Microsecond)
+		ch := time.After(time.Duration(val) * time.Millisecond)
 		<-ch
 		to <- val
 	}()
@@ -20,12 +32,8 @@ func main() {
 	for i := 0; i < 10; i++ {
 		randints = append(randints, rand.Intn(100))
 	}
-	gather := make(chan int)
-	for _, num := range randints {
-		emit(num, gather)
-	}
 
-	for x := 0; x < 10; x++ {
-		fmt.Println(<-gather)
+	for _, item := range SleepSort(randints) {
+		fmt.Println(item)
 	}
 }
