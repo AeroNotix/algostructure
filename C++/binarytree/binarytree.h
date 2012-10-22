1e0abd73a5f627728c97a0126810b047ed98640f
AeroNotix <aaron.l.france@gmail.com>
2012-10-22 10:00:15 +0000

    Adhere to coding style standards

--- C++/binarytree/binarytree.h
+++ C++/binarytree/binarytree.h
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include <iostream>
 #include <vector>
-#include <stdio.h>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -33,26 +34,25 @@ namespace binarytree {
 
     template <class T>
     class BinaryTree {
-
     public:
 	BinaryTree();
-	BinaryTree(bool (*f) (T,T));
+	BinaryTree(bool (*f)(T, T));
 	~BinaryTree();
 	void Add(T);
 	vector<T> Walk() const;
 	unsigned int Levels() const;
 	string ToString() const;
-	bool (*fcn) (T, T);
+	bool (*fcn)(T, T);
 
     private:
 	unsigned int levels;
 	Tree<T> *root;
 	void add(T i, Tree<T>* &leaf);
-	vector<T> walk(Tree<T> *leaf, vector<T>&) const;
+	void walk(Tree<T> *leaf, vector<T>*) const;
     };
 
     template <class T>
-    BinaryTree<T>::BinaryTree(bool (*f) (T,T))
+    BinaryTree<T>::BinaryTree(bool (*f)(T, T))
 	: fcn(f), root(nullptr), levels(0) {}
 
     template <class T>
@@ -93,7 +93,6 @@ namespace binarytree {
 
     template <class T>
     void BinaryTree<T>::add(T i, Tree<T>* &leaf) {
-
 	if (leaf == nullptr) {
 	    leaf = NewNode<T>(i);
 	    return;
@@ -113,35 +112,32 @@ namespace binarytree {
 
     template <class T>
     vector<T> BinaryTree<T>::Walk() const {
-
 	vector<T> out;
 	if (root == nullptr) {
 	    return out;
 	}
 
-	walk(root->Left, out);
+	walk(root->Left, &out);
 	out.push_back(root->Value);
-	walk(root->Right, out);
+	walk(root->Right, &out);
 
 	return out;
     }
 
     template <class T>
-    vector<T> BinaryTree<T>::walk(Tree<T> *leaf, vector<T>& out) const {
+    void BinaryTree<T>::walk(Tree<T> *leaf, vector<T> *out) const {
 	if (leaf == nullptr) {
-	    return out;
+	    return;
 	}
 
 	walk(leaf->Left, out);
-	out.push_back(leaf->Value);
+	out->push_back(leaf->Value);
 	walk(leaf->Right, out);
-
-	return out;
     }
 
     template<>
     string BinaryTree<int>::ToString() const {
-	stringstream buffer (stringstream::in);
+	stringstream buffer(stringstream::in);
 	buffer << "[ ";
 	for (auto el : Walk()) {
 	    buffer << el << " ";
