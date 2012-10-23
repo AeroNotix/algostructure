e41ff24a01f4b97ac2ff0b4de95db056bb13dd0b
AeroNotix <aaron.l.france@gmail.com>
2012-10-23 13:45:58 +0000

    Whitespace changes

--- CPython/binarytree/binarytree.cpp
+++ CPython/binarytree/binarytree.cpp
@@ -48,52 +48,52 @@ static PyObject* BinaryTree_walk(BinaryTree_obj *self) {
     return list;
 }
 
-static PyMethodDef  BinaryTree_methods[] = {
+static PyMethodDef BinaryTree_methods[] = {
     {"add", (PyCFunction) BinaryTree_add, METH_VARARGS, "Add an element to the BinaryTree"},
     {"walk", (PyCFunction) BinaryTree_walk, METH_NOARGS, "Walk the tree and return a list"},
-    {NULL}  /* Sentinel */
+    {NULL} /* Sentinel */
 };
 
 static PyTypeObject BinaryTreeType = {
     PyObject_HEAD_INIT(NULL)
-    0,                                    /* ob_size is not used anymore, docs don't explain *what* it is */
-    "binarytree.BinaryTree",              /* tp_name, string for object */
-    sizeof(BinaryTree_obj),               /* tp_basicsize, how much space to alloc */
-    0,                                    /* tp_itemsize */
-    (destructor) BinaryTree_dealloc,      /* tp_dealloc */
-    0,                                    /* tp_print */
-    0,                                    /* tp_getattr */
-    0,                                    /* tp_setattr */
-    0,                                    /* tp_compare */
-    0,                                    /* tp_repr */
-    0,                                    /* tp_as_number */
-    0,                                    /* tp_as_sequence */
-    0,                                    /* tp_as_mapping */
-    0,                                    /* tp_hash */
-    0,                                    /* tp_call */
-    BinaryTree_ToString,                  /* tp_str */
-    0,                                    /* tp_getattro */
-    0,                                    /* tp_setattro */
-    0,                                    /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                   /* tp_flags */
-    "BinaryTree object",                  /* class docstring */
-    0,		                          /* tp_traverse */
-    0,		                          /* tp_clear */
-    0,		                          /* tp_richcompare */
-    0,		                          /* tp_weaklistoffset */
-    0,		                          /* tp_iter */
-    0,		                          /* tp_iternext */
-    BinaryTree_methods,                   /* tp_methods */
-    0,                                    /* tp_members */
-    0,                                    /* tp_getset */
-    0,                                    /* tp_base */
-    0,                                    /* tp_dict */
-    0,                                    /* tp_descr_get */
-    0,                                    /* tp_descr_set */
-    0,                                    /* tp_dictoffset */
-    (initproc) BinaryTree_init,           /* tp_init */
-    0,                                    /* tp_alloc */
-    0,                                    /* tp_new */
+    0,                                 /* ob_size is not used anymore, docs don't explain *what* it is */
+    "binarytree.BinaryTree",           /* tp_name, string for object */
+    sizeof(BinaryTree_obj),            /* tp_basicsize, how much space to alloc */
+    0,                                 /* tp_itemsize */
+    (destructor) BinaryTree_dealloc,   /* tp_dealloc */
+    0,                                 /* tp_print */
+    0,                                 /* tp_getattr */
+    0,                                 /* tp_setattr */
+    0,                                 /* tp_compare */
+    0,                                 /* tp_repr */
+    0,                                 /* tp_as_number */
+    0,                                 /* tp_as_sequence */
+    0,                                 /* tp_as_mapping */
+    0,                                 /* tp_hash */
+    0,                                 /* tp_call */
+    BinaryTree_ToString,               /* tp_str */
+    0,                                 /* tp_getattro */
+    0,                                 /* tp_setattro */
+    0,                                 /* tp_as_buffer  */
+    Py_TPFLAGS_DEFAULT,                /* tp_flags  */
+    "BinaryTree  object",              /* tp_doc */
+    0,		                       /* tp_traverse  */
+    0,		                       /* tp_clear  */
+    0,		                       /* tp_richcompare  */
+    0,		                       /* tp_weaklistoffset  */
+    0,		                       /* tp_iter  */
+    0,		                       /* tp_iternext  */
+    BinaryTree_methods,                /* tp_methods  */
+    0,                                 /* tp_members  */
+    0,                                 /* tp_getset  */
+    0,                                 /* tp_base  */
+    0,                                 /* tp_dict  */
+    0,                                 /* tp_descr_get  */
+    0,                                 /* tp_descr_set  */
+    0,                                 /* tp_dictoffset  */
+    (initproc)  BinaryTree_init,       /* tp_init  */
+    0,                                 /* tp_alloc  */
+    0,                                 /* tp_new  */
 };
 
 static PyMethodDef binarytree_funcs[] = {
@@ -109,6 +109,5 @@ PyMODINIT_FUNC initbinarytree(void) {
     PyObject* m;
     m = Py_InitModule3("binarytree", binarytree_funcs, "Binary Tree wrapper module over a C++11 lib.");
     Py_INCREF(&BinaryTreeType);
-
     PyModule_AddObject(m, "BinaryTree", (PyObject *) &BinaryTreeType);
 }
