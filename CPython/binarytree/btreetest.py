0c7874d5644623bd26f36f8ad8b790a6253eecca
AeroNotix <aaron.l.france@gmail.com>
2012-10-25 15:54:20 +0000

    Changed CPython test code

--- CPython/binarytree/btreetest.py
+++ CPython/binarytree/btreetest.py
@@ -2,7 +2,5 @@ import binarytree
 import random
 
 x = binarytree.BinaryTree()
-for i in range(500000):
-    x.add(random.randint(-10000,i+1))
-
-print x.walk()
+for i in range(1000000):
+    x.add(random.randint(0,1000000))
