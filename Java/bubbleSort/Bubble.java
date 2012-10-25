529d5960532b61a0d3219cfef49ae240b65242ad
Gorah <elon.aep.delin@gmail.com>
2012-10-25 13:29:38 +0200

    generics added

--- Java/bubbleSort/Bubble.java
+++ Java/bubbleSort/Bubble.java
@@ -3,12 +3,12 @@ package bubbleSort;
 
 public class Bubble {
 //method #1: with 2 nested for loops
-    public static void sortBubble(int[] arr, int len){
-        int tmp = 0;
+    public static void sortBubble(Comparable[] arr, int len){
+        Comparable tmp = 0;
         for(int i = 0; i<len; i++){
             
             for(int j=1; j<(len); j++){
-                if(arr[j-1] > arr[j]){
+                if(arr[j-1].compareTo(arr[j]) == 1){
                     tmp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = tmp;
@@ -18,15 +18,15 @@ public class Bubble {
         
     }
 //method #2: with while loop    
-    public static void bubbleSort(int arr[], int n) {
+    public static void bubbleSort(Comparable[] arr, int len) {
       boolean swapped = true;
       int j = 0;
-      int tmp;
+      Comparable tmp;
       while (swapped) {
             swapped = false;
             j++;
-            for (int i = 0; i < n - j; i++) {
-                  if (arr[i] > arr[i + 1]) {
+            for (int i = 0; i < len - j; i++) {
+                  if (arr[i].compareTo(arr[i + 1]) == 1) {
                         tmp = arr[i];
                         arr[i] = arr[i + 1];
                         arr[i + 1] = tmp;
@@ -36,11 +36,21 @@ public class Bubble {
       }
 }
     public static void main(String[] args){
-        int[] arr = {3, 5, 1, 13, 4, 8, 7, 9};
+        Comparable[] arr = {3, 5, 1, 13, 4, 8, 7, 9};
+        Comparable[] arr2 = {"a", "bbb", "aa", "zzzzzz", "a"};
         int len = arr.length;
-        sortBubble(arr, len);
-//        bubbleSort(arr, len);
-        for(int i:arr){
+ //       sortBubble(arr, len);
+        bubbleSort(arr, len);
+        for(Comparable i:arr){
+            System.out.println(i);
+        }
+        
+         System.out.println();
+         
+         int len2 = arr2.length;
+ //        sortBubble(arr2, len2);
+        bubbleSort(arr2, len2);
+        for(Comparable i:arr2){
             System.out.println(i);
         }
     }
