b14e5c8b672b8a0d2c5023fe10ee37b6bfbb7bbd
AeroNotix <aaron.l.france@gmail.com>
2012-10-25 15:53:05 +0000

    Changed C++ test code.

--- C++/binarytree/test.cpp
+++ C++/binarytree/test.cpp
@@ -8,17 +8,4 @@ int main(int argc, char* argv[]) {
 	n.Add(rand() % 1000000);
     }
     n.Walk();
-    /*
-    if (argc < 2)
-	return 0;
-
-    binarytree::BinaryTree<std::string> n2;
-    std::fstream fs(argv[2], std::fstream::in);
-    while (!fs.eof()) {
-	std::string s;
-	getline(fs,s);
-	n2.Add(s);
-    }
-    n2.Walk();
-    */
 }
