5ab11d2c29fa43bf5f218931cf08c3201933da2e
Aaron France <aaron.l.france@gmail.com>
2014-04-26 21:19:27 +0200

    Add OCaml binary tree.

---
+++ OCaml/binarytree.ml
@@ -0,0 +1,24 @@
+open Core.Std
+
+
+type 'a tree =
+  | Empty
+  | Node of 'a * 'a tree * 'a tree
+
+let empty = Empty
+
+let new_tree v = Node (v, Empty, Empty)
+
+let rec insert n v =
+  match n with
+  | Empty -> new_tree v
+  | Node (ov, l, r) when ov > v ->
+     Node (ov, insert l v, r)
+  | Node (ov, l, r) ->
+     Node (ov, l, insert r v)
+
+let rec walk n =
+  match n with
+  | Empty -> []
+  | Node (v, l, r) ->
+     walk l @ [v] @ walk r
